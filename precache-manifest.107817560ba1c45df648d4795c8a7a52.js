bbb6231fd329f79bdb82b7ccfec26b40ffe1d18e
System Administrator <root@adminui-MacBook-Pro.local>
2019-03-02 12:28:55 +0900

    Updates

---
+++ precache-manifest.107817560ba1c45df648d4795c8a7a52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a78b8678fe21857201b8",
+    "url": "/Movie-app/static/css/main.41f24bf4.chunk.css"
+  },
+  {
+    "revision": "a78b8678fe21857201b8",
+    "url": "/Movie-app/static/js/main.a78b8678.chunk.js"
+  },
+  {
+    "revision": "3c630e89029e03c5eb0b",
+    "url": "/Movie-app/static/js/runtime~main.3c630e89.js"
+  },
+  {
+    "revision": "24dde547527b2142620d",
+    "url": "/Movie-app/static/js/2.24dde547.chunk.js"
+  },
+  {
+    "revision": "1314f180fd7a495e2a3b633c02c5ab79",
+    "url": "/Movie-app/index.html"
+  }
+];
